4f52ef645bb262bdfbb63c45c9253124bef05bf4
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2020-08-31 16:45:01 -0300

    Adiciona arquivo ApiDataViewController.swift

---
+++ Espasurdo/ApiDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ApiDataViewController.swift
+//  Espasurdo
+//
+//  Created by José Henrique Fernandes Silva on 31/08/20.
+//  Copyright © 2020 Morgana Beatriz. All rights reserved.
+//
+
+import UIKit
+
+class ApiDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
