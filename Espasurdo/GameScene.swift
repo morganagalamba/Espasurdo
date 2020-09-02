33bd61c97784de998bfed8018a6ffd0fc05cbc11
Morgana Galamba <morgana.feijo@gmail.com>
2020-09-02 18:03:42 -0300

    ball moving right and maze in position

--- Espasurdo/GameScene.swift
+++ Espasurdo/GameScene.swift
@@ -25,14 +25,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var R : Double = 40.0
     var newX : CGFloat = 0.0
     var newY : CGFloat = 0.0
+   var bateu = false
     
     override func sceneDidLoad() {
         
         self.lastUpdateTime = 0
         physicsWorld.contactDelegate = self
         ball = self.childNode(withName: "ball")!
-        
-        
 
     }
     
@@ -41,6 +40,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if let node = contact.bodyA.node?.name {
             if(node  == "wall"){
                 print("bateu")
+                self.bateu = true
             }
         }
             
@@ -48,11 +48,25 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             
             if(node == "wall"){
                 print("bateu")
+                self.bateu = true
+            }
+         }
+        
+        if let node = contact.bodyA.node?.name {
+            if(node  == "end"){
+                print("conseguiu")
+            }
+        }
+            
+        if let node = contact.bodyB.node?.name {
+            
+            if(node == "end"){
+                print("conseguiu")
             }
          }
     }
     
-    func updateBall(_ roll: Double,_ pitch : Double,_ accZ: Double){
+    func updateBall(_ roll: Double,_ pitch : Double){
        X = X + (2 * roll)
        Y = Y + (2 * pitch)
         
@@ -65,72 +79,28 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
            
         newX = fmin(320, fmax(-320, newX))
         newY = fmin(640, fmax(-640, newY))
+
+        ball.run(SKAction.moveTo(x: newX, duration: 0.2))
+        ball.run(SKAction.moveTo(y: -newY, duration: 0.2))
         
-        let newR : CGFloat = CGFloat(R + 10 * accZ)
-        //print("x:")
-        //print(newX)
-        //print("y:")
-        //print(newY)
-        //print("R:")
-        //print(TimeInterval(newR))
-        //ball.run(SKAction.moveTo(x: newX, y: newY, duration: 0.2))
-        ball.run(SKAction.moveTo(x: newX, duration: 1.0))
-        ball.run(SKAction.moveTo(y: -newY, duration: 1.0))
+       
     }
     
     override func update(_ currentTime: TimeInterval) {
         
-        var pitch: Double!
-        var roll: Double!
-        var yaw: Double!
-        var accZ : Double!
+        var pitch: Double = -590.0
+        var roll: Double = -300.0
         
-        if motionManager.isDeviceMotionAvailable {
-            self.motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
-            self.motionManager.showsDeviceMovementDisplay = true
-            self.motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
-            self.motionManager.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
-            self.motionManager.startAccelerometerUpdates()
-            
-            // Configure a timer to fetch the motion data.
-            self.timer = Timer(fire: Date(), interval: (1.0 / 60.0), repeats: true,
-                            block: { (timer) in
-                                if let data = self.motionManager.deviceMotion {
-                                    // Get the attitude relative to the magnetic north reference frame.
-                                   pitch = data.attitude.pitch
-                                   roll = data.attitude.roll
-                                   //yaw = data.attitude.yaw
-                                    let data = self.motionManager.accelerometerData
-                                    accZ = data?.acceleration.z
-                                    self.updateBall(roll, pitch,accZ)
-                                    // Use the motion data in your app.
-                                }
-            })
-            
-            // Add the timer to the current run loop.
-            RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
+        self.motionManager.deviceMotionUpdateInterval = 1.0 / 60.0
+        self.motionManager.showsDeviceMovementDisplay = true
+        self.motionManager.startDeviceMotionUpdates(using: .xMagneticNorthZVertical)
+       
+        if let data = self.motionManager.deviceMotion{
+            pitch = data.attitude.pitch
+            roll = data.attitude.roll
+            updateBall(roll, pitch)
         }
         
-        /*if self.motionManager.isAccelerometerAvailable {
-           self.motionManager.accelerometerUpdateInterval = 1.0 / 60.0  // 60 Hz
-           self.motionManager.startAccelerometerUpdates()
-
-           // Configure a timer to fetch the data.
-           self.timer = Timer(fire: Date(), interval: (1.0/60.0),
-                 repeats: true, block: { (timer) in
-              // Get the accelerometer data.
-              if let data = self.motionManager.accelerometerData {
-                 accZ = data.acceleration.z
-
-                 // Use the accelerometer data in your app.
-              }
-           })
-
-           // Add the timer to the current run loop.
-            RunLoop.current.add(self.timer!, forMode: RunLoop.Mode.default)
-        }*/
-        
-        //updateBall(roll, pitch)
         
     }
     
--- Espasurdo/GameViewController.swift
+++ Espasurdo/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
@@ -36,6 +36,7 @@ class GameViewController: UIViewController {
                     view.presentScene(sceneNode)
                     
                     view.ignoresSiblingOrder = true
+
                     
                     view.showsFPS = false
                     view.showsNodeCount = false
